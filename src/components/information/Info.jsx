148e7d6365e0c3b11af515b5e1c8e2f0709c5197
Museum of candy <dmaheen4037@gmail.com>
2024-12-21 21:43:54 +0530

    essageNew commit

---
+++ src/components/information/Info.jsx
@@ -0,0 +1,12 @@
+import "./Info.css"
+function Info(){
+    return(
+        <div id="Info">
+            <div className="Content">
+      <h1>Heloo</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Info;
